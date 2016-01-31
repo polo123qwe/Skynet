48737adc51b9b4a43b7d729fcc0d12a8cf1d0788
Amery <kirinohaa@gmail.com>
2016-01-31 01:13:32 -0300

    Fixed an error that I made while fixing a typo.

--- bin/bot.js
+++ bin/bot.js
@@ -56,7 +56,7 @@ mybot.on("message", function(message){
 
 // On New Member
 mybot.on("serverNewMember", function(server, user){
-	mybot.sendMessage(server.defaultChannel, "Welcome to **Anime Discord** "+user.mention()+". It is highly recommended to read through the <#137105484040634368>. Remember to type `getMembership!` to be set as a member!"
+	mybot.sendMessage(server.defaultChannel, "Welcome to **Anime Discord** "+user.mention()+". It is highly recommended to read through the <#137105484040634368>. Remember to type `getMembership!` to be set as a member!");
 })
 
 // On Status Change
