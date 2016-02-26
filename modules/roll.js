8a0fcc8e179f82a4c0c824ab960bdfa3bdca4cf2
Sergi <sergibernaus@yahoo.es>
2016-02-26 23:54:02 +0100

    Fixed Roll

--- modules/roll.js
+++ modules/roll.js
@@ -3,15 +3,16 @@
 module.exports = {
 	//here goes the name of the function
 	"roll": function(message, splitted, mybot){	
-		
-		var num;
-		if(splitted[1] == null || typeof splitted[1] != number) num = 10;
-		
+
+		var num = splitted[1];
+		if(num == null || parseInt(num) == NaN || num < 1){
+			num = 10;
+		}
 		return message.author.name+", you have rolled "+Math.floor((Math.random() * num) + 1); 
  	},
 	//What to return on help
 	"aid": function(){
-		return "This command has no help";
+		return "`roll! num` retruns a number between 1 and num";
 	},
 	//Power needed to execute the command
 	"power": 0,
